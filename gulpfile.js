a4882f7f31b918422e8dcc9d4bd485b2edbb5e5a
braddschick <bradd.schick@gmail.com>
2015-08-28 22:36:06 -0400

    Added Gulp control and bumping

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// dependencies
+var path = require('path'),
+    gulp = require('gulp'),
+    git = require('gulp-git'),
+    bump = require('gulp-bump'),
+    filter = require('gulp-filter'),
+    tag_version = require('gulp-tag-version'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    gutil = require('gulp-util');
+
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+        // bump the version number in those files
+        .pipe(bump({type: importance}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        .pipe(git.commit('bumps package version'))
+
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        .pipe(tag_version());
+}
+
+gulp.task('clean', function () {
+  return gulp.src('build', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('mini', function() {
+  return gulp.src('js/*.js')
+    .pipe(concat('gChart.js'))
+    .pipe(gulp.dest('build'))
+    .pipe(uglify())
+    .pipe(rename('gChart.min.js'))
+    .pipe(gulp.dest('build'))
+    .on('error', gutil.log)
+});
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
+
